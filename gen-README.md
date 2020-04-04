bbd785d7c25d1468fb36a9724c750d43dabd6eec
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-04-03 20:53:27 -0500

    added questions

--- gen-README.md
+++ gen-README.md
@@ -1,2 +1,19 @@
-## Contributors
+# test
+  
+  ## Contributors
   @r
+  
+  ## Description
+  * Having fun.
+  
+  ## Technologies
+  * Node.js, Axios, Inquirery
+  
+  ## Contact
+  
+  #### Rogerson Jean-Charles (@rogerson253)
+  * Email: [rogerson253@gmail.com](rogerson253@gmail.com)
+  * LinkedIn: https://www.linkedin.com/in/rogerson-jean-charles253/
+  
+  ## License
+  MIT
--- index.js
+++ index.js
@@ -8,12 +8,41 @@ inquirer
       message: "What is your github username?",
       name: "username"
     },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "project"
+    },
+    {
+      type: "input",
+      message: "Describe your project.",
+      name: "description"
+    },
   ])
   .then(function(response) {
     const username = response.username;
+    const projectName = response.project;
+    const description = response.description;
 
-    const readMe = `## Contributors
-  @${username}`
+    const readMe = `# ${projectName}
+  
+  ## Contributors
+  @${username}
+  
+  ## Description
+  * ${description}.
+  
+  ## Technologies
+  * Node.js, Axios, Inquirery
+  
+  ## Contact
+  
+  #### Rogerson Jean-Charles (@rogerson253)
+  * Email: [rogerson253@gmail.com](rogerson253@gmail.com)
+  * LinkedIn: https://www.linkedin.com/in/rogerson-jean-charles253/
+  
+  ## License
+  MIT`
 
    
    

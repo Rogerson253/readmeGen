92c63cc8fc49eba2bc431f55b92387d564e7e04d
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-04-04 16:32:54 -0500

    creted .env file to protect api key

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const axios = require("axios");
 const api = {
   getUser(username) {
     axios
     .get(`https://api.github.com/users/${username}`, 
     {
-        headers: {"Authorization": "token 729e00978cfc5dc147802e7c60be193eba9122b1"}
+        headers: {"Authorization": `token ${process.env.GH_TOKEN}`}
     }) 
     .then(response => {
         console.log(response.data.avatar_url);
